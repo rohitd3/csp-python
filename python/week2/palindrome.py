45d9edbbde594eece8180b7f4326d552cc5d3493
rohit de <44128572+rohitd3@users.noreply.github.com>
2022-04-04 22:13:29 +0000

    e

--- python/week2/palindrome.py
+++ python/week2/palindrome.py
@@ -4,10 +4,13 @@ class Palindrome:
 
     def __call__(self):
         text_strip = list([val for val in self.text if val.isalpha() or val.isnumeric()])
+      # checking to see if numbers or alphabets
         self.text = "".join(text_strip)
         self.text = self.text.lower()
+      # making the alphabets into lower case
 
         if self.text == self.text[::-1]:
+          # checking by reversing the text or numeric pattern
             return True
         else:
             return False
@@ -15,9 +18,6 @@ class Palindrome:
 test_cases = ["A man, a plan, a canal Panama!", "qwerrewq", "lmao", "racecar", "rohitttttrohit", "lol"]
 
 def tester():
-    try:
-        for n in test_cases:
-            palindrome = Palindrome(text=n)
-            print(n, palindrome())
-    except:
-        print("Sorry, something went wrong.")
+  for n in test_cases:
+      palindrome = Palindrome(text=n)
+      print(n, palindrome())
